45bbb662d06858451d5840ae901d3039d4ffadf6
Nguyen Hai Dang <haidang.4513@gmail.com>
2014-01-21 17:57:41 +0900

    web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
-var buffer = new Buffers(100);
+var buffer = new Buffer(100);
 buffer.write(fs.readFileSync("index.html"));
 var str = buffer.toString('utf-8');
 
